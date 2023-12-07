b766942b23719964292b19b51902f34f538f844f
FelipeNegrucci <felipe.negrucci@protonmail.com>
2023-12-07 13:00:32 -0300

    Defining the base of the header component for primary navigation

---
+++ vue-gym-site/src/components/compHeader.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="row justify-content-between gx-0 h-100 align-items-center">
+
+        <div class="col-auto border">
+            <div class="row gx-0">
+                <div class="col-auto">brand_logo</div>
+                <div class="col-auto ms-3">brand_name</div>
+            </div>
+        </div>
+
+        <div class="col-auto border">
+            <div class="row gx-0">
+                <div class="col-auto">primary-navigation</div>
+                <div class="col-auto ms-3">toggle-button</div>
+            </div>
+        </div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>
