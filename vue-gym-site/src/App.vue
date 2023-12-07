afc1a3ddbc58a684ab862dd2ff87ca6bb9cf8771
FelipeNegrucci <felipe.negrucci@protonmail.com>
2023-12-07 19:43:54 -0300

    Added logo and access button icons. Changed the structure of the navigation bar. Now using vue-router for the links. Created an object to better control the size of the icons.

--- vue-gym-site/src/App.vue
+++ vue-gym-site/src/App.vue
@@ -3,17 +3,19 @@ import compHeader from './components/compHeader.vue'
 
 import { ref } from 'vue';
 
-const headerHeight = ref('48px')
+const headerHeight = ref('62px')
+
+
 </script>
 
 <template>
-    <header class="container-fluid px-3 text-bg-dark" :style="{ height: headerHeight }">
+    <header class="container-fluid px-3 position-absolute" :style="{ height: headerHeight }">
         <div class="container-xxl px-0 h-100">
-            <compHeader/>
+            <compHeader class="text-light"/>
         </div>
     </header>
-    <main>
-
+    <main class="container-fluid px-0">
+        
     </main>
     <footer>
 
@@ -21,5 +23,9 @@ const headerHeight = ref('48px')
 </template>
 
 <style lang="scss">
-
+header{
+    background: rgb(43,45,66);
+    background: linear-gradient(0deg, rgba(43,45,66,0.3) 0%, rgba(43,45,66,0.5) 64%, rgba(43,45,66,0.8) 100%);
+    border-bottom: 1px solid rgba(43,45,66,0.3);
+}
 </style>
